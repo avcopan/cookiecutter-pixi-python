1706ce8bad21701eb1f57ee0dd23c7aebceba3fb
avcopan <avcopan@gmail.com>
2026-01-16 16:52:39 -0500

    Format, lint, type-check, and test the cookiecutter

---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,3 @@
+"""{{cookiecutter.package_name}}."""
+
+__version__ = "0.1.0"
---
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -0,0 +1,8 @@
+"""{{cookiecutter.package_name}} tests."""
+
+import {{cookiecutter.package_name}}
+
+
+def test_stub() -> None:
+    """Stub test to ensure the test suite runs."""
+    print({{cookiecutter.package_name}}.__version__)  # noqa: T201
