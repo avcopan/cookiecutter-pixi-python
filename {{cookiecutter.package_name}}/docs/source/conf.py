e128137a72e72ff12939f25fac5dca35ebf70c67
avcopan <avcopan@gmail.com>
2026-01-20 10:48:30 -0500

    Add API documenation with autodoc2

--- {{cookiecutter.package_name}}/docs/source/conf.py
+++ {{cookiecutter.package_name}}/docs/source/conf.py
@@ -10,6 +10,7 @@ release = metadata.version("{{cookiecutter.package_name}}")
 # 2. General configuration
 extensions = [
     "myst_parser",
+    "autodoc2"
 ]
 templates_path = []
 exclude_patterns = []
@@ -17,3 +18,18 @@ exclude_patterns = []
 # 3. Options for HTML output
 html_theme = "pydata_sphinx_theme"
 html_static_path = []
+html_theme_options = {
+    "github_url": "https://github.com/{{cookiecutter.github_org_name}}/{{cookiecutter.package_name}}",
+}
+
+# 4. Configure extensions
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "fieldlist",
+]
+
+# 5. Autodoc2 configuration
+autodoc2_packages = [
+    "../../src/{{cookiecutter.package_name}}",
+]
