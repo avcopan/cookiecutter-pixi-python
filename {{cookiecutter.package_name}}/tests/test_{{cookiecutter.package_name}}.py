58f3e679742656f82989c995418213568d9f97a2
avcopan <avcopan@gmail.com>
2026-01-20 11:01:01 -0500

    Add import layering enforcement with lint-imports

--- {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -6,3 +6,13 @@ import {{cookiecutter.package_name}}
 def test_stub() -> None:
     """Stub test to ensure the test suite runs."""
     print({{cookiecutter.package_name}}.__version__)  # noqa: T201
+
+
+def test__greet() -> None:
+    """Test the greet function."""
+    assert {{cookiecutter.package_name}}.greet("World") == "Hello, World!"
+
+
+def test__greet_jim() -> None:
+    """Test the greet_jim function."""
+    assert {{cookiecutter.package_name}}.greet_jim() == "Hello, Jim!"
